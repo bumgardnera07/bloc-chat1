dcb1fa1ebec3a478975893a74b6c8ccc9a11d8b8
George Bumgardner <bumgardnera07@gmail.com>
2017-07-25 20:17:29 -0400

    everythign works

--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -1,15 +1,25 @@
 (function() {
-  function Message($firebaseArray) {
+  function Message($firebaseArray, $cookies, $filter, Room) {
     var Message = {};
     var ref = firebase.database().ref().child('messages');
     var messages = $firebaseArray(ref);
-
+    var currenttime = $filter('date')(new Date(),'hh:mm:ss');
     Message.all =messages;
+    Message.currentRoom = Room.currentRoom;
 
 
     Message.getByRoomId = function(roomId) {
       Message.currentMessages = $firebaseArray(ref.orderByChild('roomId').equalTo(roomId));
-    }
+    };
+
+    Message.send = function(newMessage) {
+      messages.$add({
+          content: newMessage,
+          roomId: Room.currentRoom.$id,
+          sentAt: currenttime,
+          username: $cookies.get('blocChatCurrentUser'),
+          })
+    };
 
 
     return Message;
@@ -17,5 +27,5 @@
 
   angular
     .module('blocChat1')
-    .factory('Message', ['$firebaseArray', Message]);
+    .factory('Message', ['$firebaseArray', '$cookies', '$filter', 'Room', Message]);
 })();
